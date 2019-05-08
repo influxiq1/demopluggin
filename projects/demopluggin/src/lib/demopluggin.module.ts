c617a2ec042c5074260cb5e72bfa3d8bf49ce7a3
influxiq1 <himadribar.influxiq@gmail.com>
2019-05-07 23:59:52 -0400

    jk

--- projects/demopluggin/src/lib/demopluggin.module.ts
+++ projects/demopluggin/src/lib/demopluggin.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DemoplugginComponent } from './demopluggin.component';
 import { HeaderComponent } from './header/header.component';
+import {DemoMaterialModule} from './materialmodules';
+import { ButtonComponent} from './button.component';
+import {CheckboxComponent} from './checkbox.component';
+import {DateComponent} from './date.component';
+import {DynamicFieldDirective} from './dynamic-field.directive';
+import {InputComponent} from './input.component';
+import {SelectComponent} from './select.component';
+import {RadiobuttonComponent} from './radiobutton.component';
 
 @NgModule({
-  declarations: [DemoplugginComponent, HeaderComponent],
-  imports: [
+  declarations: [DemoplugginComponent,
+    HeaderComponent, ButtonComponent,
+    CheckboxComponent, DateComponent, DynamicFieldDirective, InputComponent, SelectComponent, RadiobuttonComponent],
+  imports: [DemoMaterialModule, FormsModule, ReactiveFormsModule,
   ],
   exports: [DemoplugginComponent, HeaderComponent]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {DemoplugginModule} from '../../projects/demopluggin/src/lib/demopluggin.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import {DemoplugginModule} from '../../projects/demopluggin/src/lib/demopluggin.
     imports: [
         BrowserModule,
         AppRoutingModule,
-        DemoplugginModule
+        DemoplugginModule,
+        BrowserAnimationsModule
     ],
   providers: [],
   bootstrap: [AppComponent]
